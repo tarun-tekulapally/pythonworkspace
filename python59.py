b3756e048ee1ac038ffc5f71197c1d9260627bed
tarun-tekulapally <162097154+tarun-tekulapally@users.noreply.github.com>
2024-03-04 14:33:48 +0530

    Add files via upload

---
+++ python59.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Sample Question print - sachin t - shirt number is 10
+name = "sachin"
+t_shirt = 10
+# Your Answer
+
+print(name,"t-shirt number is",t_shirt)
+
+
+# In[13]:
+
+
+# 1 . Question | print - India is number 1 team and has won 2012 worldcup and it rank 1 and the captain name is kholi
+team = "india"
+worldcup = 2012
+captain = "kholi"
+rank = 1
+print(team,'is','number',rank,'team has won',worldcup,'worldcup and it rank',rank,'and the captain name is',captain)
+
+
+
+
+
+# In[8]:
+
+
+# 2 . Question |print the datatype of the x,y,z
+
+x = 56
+y = 56.78
+z = "python"
+
+# your Answer
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+
+
+
+# In[9]:
+
+
+# 3 . Question | write a program to print 10.0 using typecasting
+
+a = 10
+b=float(a)
+print(b)
+# Your Answer
+
+
+
+
+
+
+# In[10]:
+
+
+# 4 . Question | write a program to print 12 using typecasting
+
+a = 12.56
+b=int(a)
+print(b)
+# Your Answer
+
+
+
+
+# In[11]:
+
+
+# 5 . write a program print 30 by using typecasting conncept
+
+a = "10"
+b = "20"
+c=int(a)
+d=int(b)
+e=c+d
+print(e)
+
+# Your Answer
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
